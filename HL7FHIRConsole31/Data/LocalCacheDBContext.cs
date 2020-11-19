ca1f69139063559b3b1323b0349b864c74cb64fa
Jesper Rosholm Tørresø <jrt@ase.au.dk>
2020-11-19 11:55:59 +0100

    Mindre kommentar tekst ændringer

--- HL7FHIRConsole31/Data/LocalCacheDBContext.cs
+++ HL7FHIRConsole31/Data/LocalCacheDBContext.cs
@@ -13,17 +13,7 @@ namespace HL7FHIRClient.Data
 
     public class LocalCacheDBContext : DbContext
     {
-        //protected LocalCacheDBContext() /*: base()*/
-        //{
-
-        //    //When migrations is not enabled i the database use make sure the datase is created
-        //    //Database.EnsureCreated();
-
-        //    //Following is Exlusive to "Data base.EnsureCreated();" and is used when Migration is enable and a at least one migration is added to the propject
-        //    //Database.Migration();
-
-        //}
-
+       
         public DbSet<BPMCompleteSequence> BPMCompleteSequences { get; set; }
         public DbSet<BPMLocalSampleSequence> BPMLocalSampleSequences { get; set; }
 
--- HL7FHIRConsole31/Program.cs
+++ HL7FHIRConsole31/Program.cs
@@ -13,7 +13,7 @@ namespace HL7FHIRClient
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Test af HL7FHIR!");
+            Console.WriteLine("Test of HL7FHIR!");
             //Step 1 Get acces to a local SQLite database
             using (var localdbcache = new LocalCacheDBContext())
             {
@@ -21,15 +21,16 @@ namespace HL7FHIRClient
             }
 
             //Step 2 simulation af  a time limited session with collection of BPMs
-            //In this template we are using a 1:Many association between the BMPCompleteSequence and the BMPLocalSampleSequence
-            //Here each step simualed covers 1 second with 50 sample values 
+            //In this template we are using a 1:Many association between the BMPCompleteSequence (root) and the BMPLocalSampleSequence (child).
+            //Use your own classes here
+            //Here next, each step simualed covers 1 second with 50 sample values 
             using (var localdbcache = new LocalCacheDBContext())
             {
                 var completeseq = new BPMCompleteSequence() { NameOfObject = "231145-2341", StartTime = DateTime.Now, BPMCounts = 3600, DurationInSeconds = 3600 };
                 localdbcache.Add(completeseq); //Add the root object to db context
                 localdbcache.SaveChanges(); //Save root to database
                 //Start receiving sample sequence from RPI, here the data is generated 
-                for (var step = 0; step < 100; step++) //Simulation 1 hour in steps of second
+                for (var step = 0; step < 100; step++) //Simulation 1 hour in steps of second, though here truncated to 100 steps for speding up test
                 {                    
                     var samples = new BPMLocalSampleSequence() { NoBPMValues = 50, SequenceNo = step };
                     float[] data = new float[50];
@@ -47,15 +48,15 @@ namespace HL7FHIRClient
                 }
 
                 //Step 3 when session is ended save all collected BPMs in one HL7FHIR Obersevation in the public HL7FHIR database
-                //But first retrieve complete sequence from local cache
-                
+                //But first retrieve complete sequence from local cache (!!!! not using the other instance completeseq)
+
                 var retId = completeseq.BPMCompleteSequenceId;
                 var retrievedseq = localdbcache.BPMCompleteSequences.
                     Where(i => i.BPMCompleteSequenceId.Equals(retId)).
                     Include(c => c.SequenceOfBPMSamples).Single();
                     //.ToListAsync();
 
-                //Then save sequence 
+                //Then save sequence at 
                 var hl7fhirclient = new HL7FHIRR4BPMClient();
                 var returnedid = hl7fhirclient.CreateHl7FHIRBMPObservation(retrievedseq);
 
@@ -63,7 +64,7 @@ namespace HL7FHIRClient
                 //Step 4 Get a specfic Obesrvation from the public HL7FHIR database
                 //And convert the BPM data to useable datatypes
 
-                //Step 5 optional 
+                //Step 5 optional updates and deletes
 
                 
             }
